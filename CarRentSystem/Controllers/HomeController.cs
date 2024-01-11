d847ec69098bf625e425860008631793365f758c
TomaszPiszczek <115466543+TomaszPiszczek@users.noreply.github.com>
2024-01-11 19:57:39 +0100

    Dodaj pliki projektów.

---
+++ CarRentSystem/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using CarRentSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CarRentSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
